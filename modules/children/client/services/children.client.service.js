a25d28f15b6416c69bb332fa62a8a99d787ac022
Christopher <cmoffitt@ufl.edu>
2016-02-20 18:15:25 -0500

    children front end template up, needs controller

---
+++ modules/children/client/services/children.client.service.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//Children service used for communicating with the children REST endpoints
+angular.module('children').factory('Children', ['$resource',
+  function ($resource) {
+    return $resource('api/children/:childrenId', {
+      childrenId: '@_id'
+    });
+  }
+]);
