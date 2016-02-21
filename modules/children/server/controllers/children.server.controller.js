e0c2b9a7bc2170ae45818fe94b21bf5c0c3ede16
Christopher <cmoffitt@ufl.edu>
2016-02-20 19:50:32 -0500

    working children module primitive front end

--- modules/children/server/controllers/children.server.controller.js
+++ modules/children/server/controllers/children.server.controller.js
@@ -8,35 +8,36 @@ var mongoose = require('mongoose'),
   _ = require('lodash');
 
 /**
- * Create a
+ * Create a 
  */
 exports.create = function (req, res) {
 
 };
 
 /**
- * Show the current
+ * Show the current 
  */
 exports.read = function (req, res) {
-  res.json(req.children);
+  res.json(req.child);
+
 };
 
 /**
- * Update a
+ * Update a 
  */
 exports.update = function (req, res) {
 
 };
 
 /**
- * Delete an
+ * Delete an 
  */
 exports.delete = function (req, res) {
 
 };
 
 /**
- * List of
+ * List of 
  */
 exports.list = function (req, res) {
   Children.find().exec(function(err, Children) {
@@ -47,3 +48,25 @@ exports.list = function (req, res) {
     }
   });
 };
+
+exports.childrenByID = function (req, res, next, id) {
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'Child is invalid'
+    });
+  }
+
+  Children.findById(id).populate('user', 'displayName').exec(function (err, child) {
+    if (err) {
+      return next(err);
+    } else if (!child) {
+      return res.status(404).send({
+        message: 'No child with that identifier has been found'
+      });
+    }
+    req.child = child;
+    next();
+  });
+};
+
--- modules/children/server/routes/children.server.routes.js
+++ modules/children/server/routes/children.server.routes.js
@@ -1,13 +1,17 @@
 'use strict';
 
 module.exports = function(app) {
-  // Routing logic
+  // Routing logic   
   // ...
   var children = require('../controllers/children.server.controller');
+
 
   app.route('/api/children')
     .get(children.list);
 
   app.route('/api/children/:childrenId')
     .get(children.read);
+
+  app.param('childrenId', children.childrenByID);
+
 };
