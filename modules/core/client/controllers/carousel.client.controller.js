83f6ff8b1aacc59f2d430623c05e1bdcaea409df
Warren Brown <warrenbrown@ufl.edu>
2016-03-17 19:34:37 -0400

    Carousel spacing fixed a little. Ready to pull request to master and then create new branch for the controller to randomly select the children from Mongo.

--- modules/core/client/controllers/carousel.client.controller.js
+++ modules/core/client/controllers/carousel.client.controller.js
@@ -12,7 +12,8 @@ angular.module('core').controller('CarouselCtrl', function ($scope) {
   $scope.addSlide = function() {
     var newWidth = 600 + slides.length + 1;
     slides.push({
-      image: 'http://lorempixel.com/' + newWidth + '/300',
+      //image: 'http://lorempixel.com/' + newWidth + '/300',
+      image: 'http://www.sustainablecambodia.org/shared/resize.asp?img=%2Fshared%2Fimages%2Fuploaded%2F4489%2EJPG&w=195&h=250',
       text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
       id: currIndex++
     });
