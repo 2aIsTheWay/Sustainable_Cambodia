3f36b3604b85a29ea82fef3d47355d204f5bc7ba
Leoncio-Kian <leoncio.kian@ufl.edu>
2016-03-19 17:52:27 -0400

    sponsorship module added

---
+++ modules/sponsorships/server/controllers/sponsorships.server.controller.js
@@ -0,0 +1,116 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Sponsorships = mongoose.model('Sponsorships'),
+  Users = mongoose.model('User'),
+  _ = require('lodash');
+
+/**
+ * Create a 
+ */
+exports.create = function (req, res) {
+  //create a new sponsorships
+  var sponsorship = new Sponsorships(req.body);
+
+  sponsorship.save(function(err) {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    } else {
+      res.json(sponsorship);
+    }
+  });
+};
+/**
+
+ * Show the current 
+ */
+exports.read = function (req, res) {
+  //read an individual sponsorship
+  res.json(req.sponsorship);
+};
+
+
+
+
+exports.delete = function (req, res) {
+  //not sure if this one is needed.
+};
+
+
+exports.list = function (req, res) {
+  Sponsorships.find().exec(function(err, Sponsorships) {
+    if (err) {
+      return res.status(400).send(err);
+    } else {
+      res.json(Sponsorships);
+    }
+  });
+};
+
+
+exports.listActive = function (req, res) {
+  //will list all active sponsorships
+  // NOTE: this has not been tested, not sure if it works.
+  var currentDate = new Date();
+  Sponsorships.find().$where(this.beginDate <= currentDate && this.endDate >= currentDate).exec(function(err, Sponsorships) {
+    if (err) {
+      return res.status(400).send(err);
+    } else {
+      res.json(Sponsorships);
+    }
+  });
+};
+
+exports.listSponsored = function (req, res) {
+  //will list sponsorships that an individual sponsor has sponsored (has to be a better way to say that)
+  Sponsorships.find({ user_id: req.userid }, function (err, sponsorships) {
+    if (err) {
+      return res.status(400).send(err);
+    } else {
+      res.json(sponsorships);
+    }
+  });
+};
+
+exports.sponsorshipsByID = function (req, res, next, id) {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'sponsorship is invalid'
+    });
+  }
+
+  Sponsorships.findById(id).populate('child_id', 'user_id', 'paymentType').exec(function (err, sponsorship) {
+    if (err) {
+      return next(err);
+    } else if (!sponsorship) {
+      return res.status(404).send({
+        message: 'No sponsorship with that identifier has been found'
+      });
+    }
+    req.sponsorship = sponsorship;
+    next();
+  });
+};
+
+exports.sponsorshipUserID = function (req, res, next, id) {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'userID is invalid'
+    });
+  }
+  Users.findById(id).populate('_id').exec(function (err, user) {
+    if (err) {
+      return next(err);
+    } else if (!user) {
+      return res.status(404).send({
+        message: 'No user with that identifier has been found'
+      });
+    }
+    req.user_id = user._id;
+    next();
+  });
+};
---
+++ modules/sponsorships/server/routes/sponsorships.server.routes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var sponsorshipPolicy = require('../policies/sponsorships.server.policy'),
+  sponsorship = require('../controllers/sponsorships.server.controller');
+
+module.exports = function(app) {
+  app.route('/api/sponsorships').all(sponsorshipPolicy.isAllowed)
+    .get(sponsorship.list)
+    .post(sponsorship.create); 
+    //.delete(sponsorship.delete);
+  app.route('/api/sponsorships/active').all(sponsorshipPolicy.isAllowed)
+    .get(sponsorship.listActive);
+
+  app.route('/api/sponsorships/:sponsorshipId').all(sponsorshipPolicy.isAllowed)
+    .get(sponsorship.read);  
+
+  app.route('/api/:userId/sponsor/sponsorships').all(sponsorshipPolicy.isAllowed)
+    .get(sponsorship.listSponsored);
+
+  app.param('sponsorshipId', sponsorship.sponsorshipsByID);
+  app.param('userId', sponsorship.sponsorshipUserID);
+};
+
