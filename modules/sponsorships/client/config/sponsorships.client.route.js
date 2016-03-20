17464acda97e1ea1140b6bf83f99d210ca9e2ad8
benchen88888888 <benchen88888888@gmail.com>
2016-03-20 14:45:04 -0400

    Set up the client structure

---
+++ modules/sponsorships/client/config/sponsorships.client.route.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Setting up route
+angular.module('sponsorships').config(['$stateProvider',
+  function ($stateProvider) {
+    // Sponsorhips state routing
+    $stateProvider
+      .state('sponsorship', {
+        abstract: true,
+        url: '/sponsorship',
+        template: '<ui-view/>'
+      })
+      .state('sponsorships', {
+        url: '',
+        templateUrl: 'modules/sponsorships/client/views/sponsorhips.client.view.html'
+      })
+      ;
+  }
+]);
