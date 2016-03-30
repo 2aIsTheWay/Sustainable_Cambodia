b5f4302af4c8f9456a2a6afdc4338b5d1d4d33c9
Warren Brown <protodrone@gmail.com>
2016-03-30 12:56:30 -0400

    WooHoo, Children injected\!

--- modules/core/client/controllers/carousel.client.controller.js
+++ modules/core/client/controllers/carousel.client.controller.js
@@ -1,19 +1,18 @@
 'use strict';
 
-//angular.module('core', ['ngAnimate', 'ui.bootstrap']);
-angular.module('core').controller('CarouselCtrl', function ($scope) {
+angular.module('core').controller('CarouselCtrl', ['$scope', 'Children', function ($scope,Children) {
   $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
   $scope.active = 0;
   var slides = $scope.slides = [];
   var currIndex = 0;
-  $scope.whb = 'Warren Hypnotoad Brown';
+
+  //console.log(Children);
 
   $scope.addSlide = function(slideChildPhotoID) {
     var newWidth = 600 + slides.length + 1;
+    console.log('add slide');
     slides.push({
-      //image: 'http://lorempixel.com/' + newWidth + '/300',
-      //image: 'http://www.sustainablecambodia.org/shared/resize.asp?img=%2Fshared%2Fimages%2Fuploaded%2F' + slideChildPhotoID + '%2EJPG&w=195&h=250',
       image: '/modules/children/client/img/uploads/' + slideChildPhotoID + '.jpg',
       text: ['My name is Phy Peach. This year 2015, I plan to begin my university studies in Management at UME in Pursat Town. Earlier Profile from 2014: I am in grade 12 at Tea Chom Rat High School. I live in Svay Att Village. Both my mother and father are farmers. I have one brother and 2 sisters. In my free time I do my homework. I want to attend school at Sustainable Cambodia because I will be able to learn computer skills, Khmer and English. Physics is my favorite subject. When I am older I want to be an electrician because I can make a lot of money and install lights in town.',
       'My name is Ravy. I am a boy in grade 6 at Sthany Primary School. My father is a security guard and my mother is a housewife. I have one brother and one sister. rnrnI like to help my parents with the housework and play with my friends in my spare time. I want to attend school at Sustainable Cambodia because I want to gain knowledge and I don&rsquo;t pay for school. What I like most about school is reading story books. My favorite subject is Math. When I grow up I want to be a teacher.',
@@ -21,6 +20,7 @@ angular.module('core').controller('CarouselCtrl', function ($scope) {
       'My name is Sreyroth. I am in the twelfth grade at Teachomrat High School. Both of my parents are farmers and I have three sisters and five brothers. I like to read books in my spare time. I want to attend school at Sustainable Cambodia because I do not have to spend money and I want to gain more knowledge and study English. What I like most about Sustainable Cambodia is the library and the gardens. My favorite subject is English because I want to be able to speak with foreigners. When I grow up I want to be a teacher because I want to teach children.'][slides.length % 4],
       id: currIndex++
     });
+    console.log($scope.slides);
   };
 
   //for (var i = 0; i < 4; i++) {
@@ -36,4 +36,4 @@ angular.module('core').controller('CarouselCtrl', function ($scope) {
   $scope.addSlide(4520);
   $scope.addSlide(4530);
   
-});
+}]);
