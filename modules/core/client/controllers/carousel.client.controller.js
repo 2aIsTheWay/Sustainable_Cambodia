b85681981dbbb5b572e105b894b6188840d53c95
Warren Brown <protodrone@gmail.com>
2016-03-30 23:53:19 -0400

    Added freindly example comment text

--- modules/core/client/controllers/carousel.client.controller.js
+++ modules/core/client/controllers/carousel.client.controller.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// The line below is where the injection happens to access Children. Note how the controller now has square brackets that open here and close at the end.
+// This means we are registering dependencies for the controller. Dragon warnings: $scope need to come first. Always. Also, the first time you see the word
+// Children it is a dependency injection and requires single quotes. It appears the second time as parameter being passed into he anonymous function and 
+// does not require single quotes.
 angular.module('core').controller('CarouselCtrl', ['$scope', 'Children', function ($scope,Children) {
   $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
@@ -7,15 +11,23 @@ angular.module('core').controller('CarouselCtrl', ['$scope', 'Children', functio
   var slides = $scope.slides = [];
   var currIndex = 0;
 
+
+  // Now that we hqve access to the Children API, we need a function to call response.data to access it and populate a variable with the results.
+  // That's what this function does. It calls the getCarousel factory which calls an $http query to route /api/children/carousel and checks authoization
+  // policy. Then the route calls the children server controller for carouselList. To understand how it all wires together, I like this notation:
+  // findCarousel --> factory --> route --> authorization policy --> server controller --> response.data --> populated var for our use (we called it children)
   $scope.findCarousel = function() {
-      Children.getCarousel().then(function(response) {
-        $scope.children = response.data;
-      }, function(error) {
-        $scope.error = 'Unable to retrieve children\n' + error;
-      });
-    };
+    Children.getCarousel().then(function(response) {
+      $scope.children = response.data;
+    }, function(error) {
+      $scope.error = 'Unable to retrieve children\n' + error;
+    });
+  };
 
 
+  
+  // This section is not relative to example of accessing child data. It created the static example slides and will be completely rewritten once
+  // Richard decides on slide content and format.
   $scope.addSlide = function(slideChildPhotoID) {
     var newWidth = 600 + slides.length + 1;
     slides.push({
@@ -28,12 +40,7 @@ angular.module('core').controller('CarouselCtrl', ['$scope', 'Children', functio
     });
   };
 
-  //for (var i = 0; i < 4; i++) {
-  //  var childPhotoID = 4490;
-  //  $scope.addSlide(childPhotoID);
-  //  childPhotoID = childPhotoID + 10;
-  //}
-
+  // More static text. Ignore for example.
   $scope.addSlide(4489);
   $scope.addSlide(4499);
   $scope.addSlide(4510);
@@ -42,3 +49,4 @@ angular.module('core').controller('CarouselCtrl', ['$scope', 'Children', functio
 
 
 }]);
+// Don't forget the closing square bracket on the line above to close the dependency injection.
