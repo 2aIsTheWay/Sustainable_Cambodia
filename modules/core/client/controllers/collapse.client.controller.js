6091ee50bf4c524b193d6bafab77ec5bc00b4ed6
Tzeryushi <rwhandel@gmail.com>
2016-04-14 01:48:01 -0400

    minor changes before I screw it up again

--- modules/core/client/controllers/collapse.client.controller.js
+++ modules/core/client/controllers/collapse.client.controller.js
@@ -3,7 +3,4 @@
 //angular.module('core', ['ngAnimate', 'ui.bootstrap']);
 angular.module('app', ['ui.bootstrap'])
 .controller('CollapseController', function($scope){
-  $scope.collapsedCC = "true";
-  $scope.collapsedPP = true;
-  $scope.collapsedCheck = true;
 });
