9873584a414405333d4adba2a038bc8fda3fbe49
Chris-Moffitt <moffitt.a.chris@gmail.com>
2016-04-14 20:16:42 -0400

    added sponsorship child photos

---
+++ modules/sponsorships/client/filters/sponsorships.client.filters.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('sponsorships')
+  .filter('split', function() {
+    return function(input, splitChar, splitIndex) {
+      // do some bounds checking here to ensure it has that index
+      return input.split(splitChar)[splitIndex];
+    };
+  });
